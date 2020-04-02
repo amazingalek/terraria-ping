305d1005d1f314954a165a37ee2161630e4492d2
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-02 23:13:47 +0200

    drawing on fullscreen map

--- PingMod.cs
+++ PingMod.cs
@@ -9,6 +9,7 @@ namespace PingMod
     public class PingMod : Mod
     {
         public static event Action OnPostDrawInterface;
+        public static event Action OnPostDrawFullscreenMap;
 
         private Projectile _ping;
 
@@ -32,68 +33,10 @@ namespace PingMod
             OnPostDrawInterface?.Invoke();
         }
 
-        //public override void HandlePacket(BinaryReader reader, int whoAmI)
-        //{
-        //    var posX = reader.ReadInt32();
-        //    var posY = reader.ReadInt32();
-        //    var pos = new Vector2(posX, posY);
-        //    Main.NewText($"Received ping from {Main.player[whoAmI].name} at {pos}");
-
-        //    Projectile.NewProjectileDirect(pos, Vector2.Zero, ProjectileType("Ping"), 0, 0, whoAmI);
-        //}
-
-        //public override void UpdateUI(GameTime gameTime)
-        //{
-        //    if (ShouldCreateWorldMapPing())
-        //    {
-        //        var mousePos = GetMousePos();
-        //        var worldMapCoords = GetWorldMapCoords(mousePos);
-        //        var pingPos = GetWorldCoords(worldMapCoords);
-        //        CreatePing(pingPos);
-        //    }
-        //    else if (ShouldCreateMiniMapPing())
-        //    {
-        //        var pingPos = GetMiniMapMousePos();
-        //        CreatePing(pingPos);
-        //    }
-        //    else if (ShouldCreateWorldPing())
-        //    {
-        //        var pingPos = GetWorldMousePos();
-        //        CreatePing(pingPos);
-        //    }
-        //}
-
-        //private Vector2 GetMousePos()
-        //{
-        //    return new Vector2(Main.mouseX, Main.mouseY);
-        //}
-
-        //private Point GetWorldMapMousePos()
-        //{
-        //    var method = typeof(CaptureInterface).GetMethod("GetMapCoords", BindingFlags.Static | BindingFlags.NonPublic);
-        //    var point = new Point();
-        //    method.Invoke(null, new object[] { (int)mouse.X, (int)mouse.Y, 0, point });
-        //    //CaptureInterface.GetMapCoords((int)mouse.X, (int)mouse.Y, 0, out point);
-        //    return point;
-        //}
-
-
-        //private bool ShouldCreateWorldMapPing()
-        //{
-        //    return Main.mapReady && Main.mapEnabled && Main.mapFullscreen && Main.mouseLeft && Main.mouseLeftRelease;
-        //}
-
-        //private void CreatePing(Point pingPos)
-        //{
-        //    throw new System.NotImplementedException();
-        //}
+        public override void PostDrawFullscreenMap(ref string mouseText)
+        {
+            OnPostDrawFullscreenMap?.Invoke();
+        }
 
     }
 }
-
-/* todo
- * ping sprite
- * ping sound
- * click on mini map: create ping
- * click on large map: create ping - see mouse hover over ore
- */
